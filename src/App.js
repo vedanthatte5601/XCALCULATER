55cd89e8e2b5821f3ff57333488972623433e50e
vedanthatte5601 <vedanthatte5601@gmail.com>
2024-03-28 22:20:15 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    setExpression((prevExpression) => prevExpression + value);
+  };
+
+  const evaluateExpression = () => {
+    console.log(expression);
+
+    if (!expression){
+      setResult("Error");
+      return
+    }
+    try {
+      const result = eval(expression);
+      if(isNaN(result)){
+        setResult('NaN')
+      }else if(!isFinite(result)){
+        setResult('Infinity');
+      }else{
+        setResult(result);
+      }
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  const clearCalculator = () => {
+    setExpression("");
+    setResult("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h1>React Calculator</h1>
+      <input
+        type="text"
+        value={expression}
+        readOnly
+        className="expression-input"
+      />
+      <div className="result">{result}</div>
+      <div className="buttons">
+       
+        <div>
+          <button onClick={() => handleButtonClick("7")}>7</button>
+          <button onClick={() => handleButtonClick("8")}>8</button>
+          <button onClick={() => handleButtonClick("9")}>9</button>
+          <button onClick={() => handleButtonClick("+")}>+</button>
+         
+        </div>
+
+        <div>
+          <button onClick={() => handleButtonClick("4")}>4</button>
+          <button onClick={() => handleButtonClick("5")}>5</button>
+          <button onClick={() => handleButtonClick("6")}>6</button>
+          <button onClick={() => handleButtonClick("-")}>-</button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick("1")}>1</button>
+
+          <button onClick={() => handleButtonClick("2")}>2</button>
+          <button onClick={() => handleButtonClick("3")}>3</button>
+           <button onClick={() => handleButtonClick("*")}>*</button>
+          
+         
+        </div>
+         <div>
+          <button onClick={clearCalculator}>C</button>
+          <button onClick={() => handleButtonClick("0")}>0</button>
+           <button onClick={evaluateExpression}>=</button>
+          <button onClick={() => handleButtonClick("/")}>/</button>
+        </div>
+      </div>
     </div>
   );
 }
